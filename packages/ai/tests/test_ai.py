d94c2919bf8386c63ffcbe8e7d23f8a471481042
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-03-26 09:40:31 -0700

    feat: integrate codecov (#12)

--- packages/ai/tests/test_ai.py
+++ packages/ai/tests/test_ai.py
@@ -1,2 +1,9 @@
-def test_ai():
-    assert True
+from ai import main
+
+
+def test_ai(capfd):
+    main()
+
+    out, err = capfd.readouterr()
+
+    assert out == "Call your main application code here\n"
