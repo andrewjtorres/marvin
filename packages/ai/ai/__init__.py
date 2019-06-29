7ef25750e458f5917ee1bf7a31bcf46520a8fe24
Andrew Torres <andrew.jonathan.torres@gmail.com>
2019-06-29 16:47:26 -0700

    refactor: update configs (#22)

--- packages/ai/ai/__init__.py
+++ packages/ai/ai/__init__.py
@@ -1,3 +1,3 @@
-def main():
-    """Entry point for the application script"""
+def main() -> None:
+    """Entry point for the application script."""
     print("Call your main application code here")
